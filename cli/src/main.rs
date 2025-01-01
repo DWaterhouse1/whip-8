eb2feb27949ee823dc90cb42916ea93ac4c82b9e
Dan Waterhouse <d.waterhouse0496@gmail.com>
2025-01-01 23:46:51 +0000

    WIP add display

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -2,8 +2,27 @@ mod commands;
 
 use crate::commands::Args;
 use clap::Parser;
+use error_iter::ErrorIter as _;
 use interpreter::processor::*;
+use log::error;
+use pixels::{Pixels, SurfaceTexture};
 use std::fs;
+use winit::dpi::LogicalSize;
+use winit::event::{Event, WindowEvent};
+use winit::event_loop::EventLoop;
+use winit::keyboard::KeyCode;
+use winit::window::WindowBuilder;
+use winit_input_helper::WinitInputHelper;
+
+const WIDTH: u32 = 320;
+const HEIGHT: u32 = 240;
+
+fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
+    error!("{method_name}() failed: {err}");
+    for source in err.sources().skip(1) {
+        error!("  Caused by: {source}");
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
@@ -24,8 +43,68 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         )
     })?;
 
-    loop {
-        proc.step().map_err(|err| format!("{}", err))?;
-        std::thread::sleep(std::time::Duration::from_millis(1));
-    }
+    env_logger::init();
+    let event_loop = EventLoop::new().unwrap();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("WHIP-8")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)?
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+
+    event_loop.run(|event, elwt| {
+        // Draw the current frame
+        if let Event::WindowEvent {
+            event: WindowEvent::RedrawRequested,
+            ..
+        } = event
+        {
+            for pixel in pixels.frame_mut().chunks_exact_mut(4) {
+                pixel.copy_from_slice(&[0x5e, 0x48, 0xe8, 0xff]);
+            }
+            if let Err(err) = pixels.render() {
+                log_error("pixels.render", err);
+                elwt.exit();
+                return;
+            }
+
+            if proc.step().is_err() {
+                elwt.exit();
+                return;
+            }
+        }
+
+        // Handle input events
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(KeyCode::Escape) || input.close_requested() {
+                elwt.exit();
+                return;
+            }
+
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                if let Err(err) = pixels.resize_surface(size.width, size.height) {
+                    log_error("pixels.resize_surface", err);
+                    elwt.exit();
+                    return;
+                }
+            }
+
+            // Update internal state and request a redraw
+            // world.update();
+            window.request_redraw();
+        }
+    })?;
+
+    Ok(())
 }
