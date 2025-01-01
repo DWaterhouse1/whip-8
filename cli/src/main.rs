86492620e5fce0744c7d0acefa0b57d359683ccd
Dan Waterhouse <d.waterhouse0496@gmail.com>
2025-01-01 21:13:36 +0000

    Add minimal CLI program.
    Added a simple binary that uses the interpreter lib. It takes a path as
    a single positional argument, and loads any file that exists at that
    path into memory. This data is then passed to the interpreter, which
    attempts to run it as a chip-8 program. No display or sound or telemetry
    of any kind are currently included.
    
    Tested on a simple test chip-8 program and observed the interpreter run
    it without encountering errors.

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,31 @@
-use interpreter::processor::*;
+mod commands;
 
-fn main() {
-    let _ = Processor::new(vec![]).unwrap();
-    println!("Hello whip 8!");
+use crate::commands::Args;
+use clap::Parser;
+use interpreter::processor::*;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    let data: Vec<u8> = fs::read(args.path.clone()).map_err(|err| {
+        format!(
+            "Error reading input file at {}: {}",
+            args.path.to_str().unwrap_or("<Invalid Path>"),
+            err
+        )
+    })?;
+
+    let mut proc = Processor::new(data).map_err(|err| {
+        format!(
+            "Error occured loading program at path {}: {}",
+            args.path.display(),
+            err
+        )
+    })?;
+
+    loop {
+        proc.step().map_err(|err| format!("{}", err))?;
+        std::thread::sleep(std::time::Duration::from_millis(1));
+    }
 }
